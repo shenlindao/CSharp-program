2e75e8825516325e8ef3bdb8766677dbbcdda98c
Artem <shenlin@yandex.ru>
2023-04-11 01:03:07 +0300

    add_lib_&_program

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Library;
+
+int sizeArray = Convert.ToInt32(GetValue("Введите количество строк, которое собираетесь добавить: "));
+
+string[] array1 = new string[sizeArray];
+string[] array2 = new string[array1.Length];
+
+FirstArray(array1);
+SecondArray(array1, array2);
+PrintArray(array2);
